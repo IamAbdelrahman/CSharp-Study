86ecd9d0dfcaaeb7ab7f5303e66de8a403eee1ee
IamAbdelrahman <abdleruhman.m.kamal@gmail.com>
2025-02-15 18:38:17 +0200

    MainProgram

--- Labs/SimpleMathEvaluator/Program.cs
+++ Labs/SimpleMathEvaluator/Program.cs
@@ -1,3 +1,16 @@
+/******************************************************************************
+ * Copyright (C) 2025 by Abdelrahman Kamal - CSharpFundamentalsCourse by M.ElMahdy
+ *****************************************************************************/
+
+/*****************************************************************************
+ * FILE DESCRIPTION
+ * ----------------------------------------------------------------------------
+ *  @file: Program.cs
+ *  @brief This file runs the application of math evaluator
+ *
+ *  @details: It asks the user to enter math expression to be evaluated and then
+ * 	the result is printed.
+ *****************************************************************************/
 namespace CSharpLabs;
 
 
